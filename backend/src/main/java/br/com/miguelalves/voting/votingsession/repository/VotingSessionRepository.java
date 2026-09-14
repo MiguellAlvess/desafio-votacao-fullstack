725dadd2b9b958eb274959e2e14ce3b9071cfcec
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-09-14 14:48:08 -0300

    feat(backend): add voting session repository

---
+++ backend/src/main/java/br/com/miguelalves/voting/votingsession/repository/VotingSessionRepository.java
@@ -0,0 +1,8 @@
+package br.com.miguelalves.voting.votingsession.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.miguelalves.voting.votingsession.domain.VotingSession;
+
+public interface VotingSessionRepository extends JpaRepository<VotingSession, Long> {
+}
