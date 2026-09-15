91602cce509d6a5dac0f15c3d1168901b31d5ede
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-09-15 13:23:33 -0300

    feat(backend): add proposal lookup to voting session repository

--- backend/src/main/java/br/com/miguelalves/voting/votingsession/repository/VotingSessionRepository.java
+++ backend/src/main/java/br/com/miguelalves/voting/votingsession/repository/VotingSessionRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.miguelalves.voting.votingsession.domain.VotingSession;
 
 public interface VotingSessionRepository extends JpaRepository<VotingSession, Long> {
+
+    boolean existsByProposalId(Long proposalId);
 }
