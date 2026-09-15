2f6afb68defdc49d0c0ace1f2230d745eb4860f7
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-09-15 11:46:58 -0300

    feat(backend): add associate controller with create method

---
+++ backend/src/main/java/br/com/miguelalves/voting/associate/controller/AssociateController.java
@@ -0,0 +1,33 @@
+package br.com.miguelalves.voting.associate.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.miguelalves.voting.associate.dto.AssociateResponse;
+import br.com.miguelalves.voting.associate.dto.CreateAssociateRequest;
+import br.com.miguelalves.voting.associate.service.AssociateService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/associates")
+public class AssociateController {
+
+    private final AssociateService associateService;
+
+    public AssociateController(AssociateService associateService) {
+        this.associateService = associateService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AssociateResponse> create(
+            @Valid @RequestBody CreateAssociateRequest request) {
+        var response = associateService.create(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
