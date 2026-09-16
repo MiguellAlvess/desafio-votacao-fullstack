2c76fe5133457efd1feb9a324f12a5a45d7b21fc
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-09-16 11:46:42 -0300

    feat(backend): add open voting sessions query

--- backend/src/main/java/br/com/miguelalves/voting/votingsession/repository/VotingSessionRepository.java
+++ backend/src/main/java/br/com/miguelalves/voting/votingsession/repository/VotingSessionRepository.java
@@ -1,5 +1,8 @@
 package br.com.miguelalves.voting.votingsession.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.miguelalves.voting.votingsession.domain.VotingSession;
@@ -7,4 +10,8 @@ import br.com.miguelalves.voting.votingsession.domain.VotingSession;
 public interface VotingSessionRepository extends JpaRepository<VotingSession, Long> {
 
     boolean existsByProposalId(Long proposalId);
+
+    List<VotingSession> findAllByStartsAtLessThanEqualAndEndsAtAfter(
+            LocalDateTime startsAt,
+            LocalDateTime endsAt);
 }
