34a4618893b580e143bf2c30c96360e74a884293
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-09-16 10:57:11 -0300

    feat(backend): add vote controller

---
+++ backend/src/main/java/br/com/miguelalves/voting/vote/controller/VoteController.java
@@ -0,0 +1,35 @@
+package br.com.miguelalves.voting.vote.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.miguelalves.voting.vote.dto.CreateVoteRequest;
+import br.com.miguelalves.voting.vote.dto.VoteResponse;
+import br.com.miguelalves.voting.vote.service.VoteService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/voting-sessions/{votingSessionId}/votes")
+public class VoteController {
+
+    private final VoteService voteService;
+
+    public VoteController(VoteService voteService) {
+        this.voteService = voteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<VoteResponse> vote(
+            @PathVariable Long votingSessionId,
+            @Valid @RequestBody CreateVoteRequest request) {
+        var response = voteService.vote(votingSessionId, request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+}
