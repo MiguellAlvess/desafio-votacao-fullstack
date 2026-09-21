5b1890ed313d9b844cece64717c0a596533519cb
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-09-21 15:10:18 -0300

    feat(frontend): add voting routes

--- frontend/src/app/routes/router.tsx
+++ frontend/src/app/routes/router.tsx
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import IdentifyPage from '@/pages/identify-page'
 import ManagementPage from '@/pages/management-page'
 import OpenSessionsPage from '@/pages/open-sessions-page'
+import VoteSuccessPage from '@/pages/vote-success-page'
+import VotingPage from '@/pages/voting-page'
 
 import AppLayout from '../layouts/app-layout'
 
@@ -19,6 +21,14 @@ export function AppRouter() {
            <Route
           path="/gestao"
           element={<ManagementPage />}
+        />
+                <Route
+          path="/votacoes/:votingSessionId"
+          element={<VotingPage />}
+        />
+        <Route
+          path="/votacoes/:votingSessionId/sucesso"
+          element={<VoteSuccessPage />}
         />
       </Route>
     </Routes>
